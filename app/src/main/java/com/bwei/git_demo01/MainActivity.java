df801115f79919947b40191afd13550493d11f9f
liumeichao <1599029306@qq.com>
2018-06-05 20:22:50 +0800

    第一次成功

---
+++ app/src/main/java/com/bwei/git_demo01/MainActivity.java
@@ -0,0 +1,29 @@
+package com.bwei.git_demo01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //获取资源ID
+        textView = findViewById(R.id.tv_textview1);
+
+        //点击事件
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"成功",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
